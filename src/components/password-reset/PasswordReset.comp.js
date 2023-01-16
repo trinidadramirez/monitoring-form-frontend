52844ff47e095b069e5292ba23156492beb3b933
trinidadramirez <tr1n1dad.ramirez@icloud.com>
2023-01-16 14:45:50 -0800

    Improved UI design for login page and added password reset form

--- src/components/password-reset/PasswordReset.comp.js
+++ src/components/password-reset/PasswordReset.comp.js
@@ -3,20 +3,14 @@ import PropTypes from "prop-types";
 import "../../App.css"
 import { Container, Row, Col, Form, Button } from "react-bootstrap";
 
-export const LoginForm = ({
-  handleOnChange,
-  handleOnSubmit,
-  switchForm,
-  email,
-  password,
-}) => {
+export const ResetPassword = ({ handleOnChange, handleOnResetSubmit, switchForm, email }) => {
   return (
     <Container>
       <Row>
         <Col>
-          <h1 className="txt-primary">SD Monitoring Login</h1>
+          <h1 className="txt-primary">Reset Password</h1>
           <hr />
-          <Form autoComplete="off" onSubmit={handleOnSubmit}>
+          <Form autoComplete="off" onSubmit={handleOnResetSubmit}>
             <Form.Group>
               <div className="text-start mt-3 text-secondary">
                 <Form.Label>Email Address</Form.Label>
@@ -29,20 +23,8 @@ export const LoginForm = ({
                 required
               />
             </Form.Group>
-            <Form.Group>
-              <div className="text-start mt-3 text-secondary">
-                <Form.Label>Password</Form.Label>
-              </div>
-              <Form.Control
-                type="password"
-                name="password"
-                value={password}
-                onChange={handleOnChange}
-                required
-              />
-            </Form.Group>
             <Button className="login-btn" type="submit">
-              Log In
+              Email New Password
             </Button>
           </Form>
         </Col>
@@ -50,9 +32,7 @@ export const LoginForm = ({
       <Row>
         <Col>
           <div className="login-btn">
-            <a href="#!" onClick={() => switchForm("reset")}>
-              Forgot Password?
-            </a>
+            <a href="#!" onClick={() => switchForm("login")}>Back to login</a>
           </div>
         </Col>
       </Row>
@@ -60,10 +40,9 @@ export const LoginForm = ({
   );
 };
 
-LoginForm.propTypes = {
+ResetPassword.propTypes = {
   handleOnChange: PropTypes.func.isRequired,
   handleOnSubmit: PropTypes.func.isRequired,
   switchForm: PropTypes.func.isRequired,
   email: PropTypes.string.isRequired,
-  password: PropTypes.string.isRequired,
 };
--- src/pages/login/Login.page.js
+++ src/pages/login/Login.page.js
@@ -1,13 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
 import "../../App.css";
-import "../../styles/Login.style.css"
-import { LoginForm } from "../../components/login/Login.comp"
+import "../../styles/login/Login.style.css";
+import { LoginForm } from "../../components/login/Login.comp";
+import { ResetPassword } from "../../components/password-reset/PasswordReset.comp";
 
 export const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [formLoad, setFormLoad] = useState("login");
+
+  const handleOnChange = (e) => {
+    const { name, value } = e.target;
+    console.log(name, value);
+
+    switch (name) {
+      case "email":
+        setEmail(value);
+        break;
+      case "password":
+        setPassword(value);
+        break;
+      default:
+        break;
+    }
+  };
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+
+    if (!email || !password) {
+      return alert("Missing email address and/or password");
+    }
+  };
+
+  const switchForm = formType => {
+    setFormLoad(formType);
+  }
+
   return (
     <div className="login-page btn-primary">
       <div className="jumbotron">
-        <LoginForm />
+        {formLoad === "login" && (
+          <LoginForm
+            handleOnChange={handleOnChange}
+            handleOnSubmit={handleOnSubmit}
+            switchForm={switchForm}
+            email={email}
+            password={password}
+          />
+        )}
+        {formLoad === "reset" && (
+          <ResetPassword
+            handleOnChange={handleOnChange}
+            handleOnSubmit={handleOnSubmit}
+            switchForm={switchForm}
+            email={email}
+          />
+        )}
       </div>
     </div>
   );
